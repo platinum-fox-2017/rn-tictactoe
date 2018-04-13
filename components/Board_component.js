05e7daf968107f7e401cdd681662a45d75de89bd
Gustaf Pahlevi <gustafpahlevi@gmail.com>
2018-04-13 11:44:03 +0700

    Update Final

--- components/Board_component.js
+++ components/Board_component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, TouchableWithoutFeedback } from 'react-native';
 import O_component from './O_component';
 
 export default class Board_component extends Component {
@@ -8,46 +8,147 @@ export default class Board_component extends Component {
         this.state = {
             activePlayer: [],
             players: ['A', 'B'],
+            playerInput: [],
+            computeInput: [],
+            result: '',
             boardData: []
         }
+    }
+
+    handlePressButton = (pos) => {
+        console.log(pos)
+        this.setState(prevState => ({
+            boardData: [...prevState.boardData, pos]
+        }))
     }
 
     render() {
         return (
         <View style={ styles.container } >
-        
-            <Text> Play the games! </Text>
-            <View style={ styles.board } >
-                <View style={ styles.line }>
-
-                </View>
-
-                <View style={[styles.line, {
-                    transform: [{
-                        translateX: 200
-                    }]
-                }]}>
-                </View>
-
-                <View style={[styles.line, {
-                    height: 3,
-                    width: 306,
-                    transform: [{
-                        translateY: 100
-                    }]
-                }]}>
-                </View>
-                
-                <View style={[styles.line, {
-                    height: 3,
-                    width: 306,
-                    transform: [{
-                        translateY: 200
-                    }]
-                }]}>
-                </View>
-
-                < O_component />
+            <Text> Play! </Text>
+            <Text> { this.state.boardData } </Text>
+            <View style={ styles.detailbox }>
+                <TouchableHighlight onPress={ () => this.handlePressButton(1) } style={styles.square}>
+                    <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 1) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(2) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 2) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(3) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 3) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+            </View>
+            <View style={ styles.detailbox }>
+                <TouchableHighlight onPress={ () => this.handlePressButton(4) } style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 4) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(5) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 5) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(6) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 6) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+            </View>
+            <View style={ styles.detailbox }>
+                <TouchableHighlight onPress={ () => this.handlePressButton(7) } style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 7) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(8) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 8) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight  onPress={ () => this.handlePressButton(9) }  style={styles.square}>
+                     <View style={ styles.selectedItem }>
+                        {
+                            this.state.boardData.map(i => {
+                                if (i === 9) {
+                                    return (
+                                        <Text style={ styles.textItem }>X</Text>
+                                    )
+                                }
+                            }) 
+                        }
+                    </View>
+                </TouchableHighlight>
             </View>
         </View>
         );
@@ -56,6 +157,7 @@ export default class Board_component extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         marginTop: 10,
         alignItems: 'center',
     },
@@ -71,6 +173,23 @@ const styles = StyleSheet.create({
         width: 3,
         position: 'absolute',
         transform : [{ translateX: 100 }]
+    },
+    square: {
+        borderWidth: 3,
+        width: 70,
+        height: 70,
+    },
+    detailbox: {
+        flexDirection: 'row',
+    },
+    selectedItem: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textItem: {
+        fontSize: 25,
+        fontWeight: 'bold',
     }
 
 });
--- components/O_component.js
+++ components/O_component.js
@@ -3,14 +3,13 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default class Circle extends Component {
     render() {
-        const { xTranslate, yTranslate, color } = this.props
+        const { xTranslate, yTranslate } = this.props
         return (
             <View style={[styles.container, {
                 transform: [
                     { translateX: xTranslate ? xTranslate : 10 },
                     { translateY: yTranslate ? yTranslate : 10 },
-                ],
-                backgroundColor: color ? color : '#000'
+                ]
             }]}>
                 <View style={styles.innerCircle}>
                 </View>
