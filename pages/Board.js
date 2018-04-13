e669dc0ef6b7f994020812436d12e9de8a678e83
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-04-13 10:30:40 +0700

    Make computer turn method

--- pages/Board.js
+++ pages/Board.js
@@ -14,54 +14,74 @@ class Board extends Component {
     this.state = {
       text: 'board',
       boardData: [[0,0,0],[0,0,0],[0,0,0]],
+      nullBoard: [],
       activePlayer: 0
     }
   }
-  
-  touchBoard = (x,y) => {
-      if (this.state.activePlayer == 0) {
-        this.state.boardData[0][0] = 1
-        this.state.activePlayer = 1
-      } else {
-        this.state.boardData[0][0] = 2
-        this.state.activePlayer = 0
+  computerTurn = ()  => {
+    const { boardData } = this.state
+    let getBoard = false
+    let nullBoard = []
+    for (var i = 0; i <  boardData.length; i++) {
+      for (var j = 0; j < boardData[i].length; j++)  {
+        if (boardData[i][j] == 0) {
+           boardData[i][j] = 2
+           getBoard = true
+          break
+        }
       }
-      const newBoard = this.state.boardData
+      if (getBoard) {
+        break; 
+      }
+    }
+  
+    this.setState({ boardData: boardData, activePlayer: 0 })
+  }
 
-      this.setState({ boardData: newBoard, activePlayer: this.state.activePlayer })
-
+  touchBoard = (x,y) => {
+    if (this.state.boardData[x][y] == 0) {
+      if (this.state.activePlayer == 0) {
+          this.state.boardData[x][y] = 1
+          this.state.activePlayer = 1
+        } else {
+          this.state.boardData[x][y] = 2
+          this.state.activePlayer = 0
+        }
+        const newBoard = this.state.boardData
+        this.setState({ boardData: newBoard, activePlayer: this.state.activePlayer })
+        this.computerTurn()
+    }      
   }
 
   render() {
     return (
       <View>
         <Text>{ this.state.text } </Text>
-        <Text>{ JSON.stringify(this.state.boardData) } </Text>
-        <Text>{ JSON.stringify(this.state.activePlayer) } </Text>
+        { this.state.activePlayer === 0 ? <Text> Your Turn </Text> : <Text> Computer Turn </Text> }
           <View style={{
           flex: 1,
           flexDirection: 'row',
           justifyContent: 'center',
           marginBottom: 40
           }}>
-          <TouchableHighlight onPress={ () => {
-            if (this.state.activePlayer == 0) {
-              this.state.boardData[0][0] = 1
-              this.state.activePlayer = 1
-            } else {
-              this.state.boardData[0][0] = 2
-              this.state.activePlayer = 0
-            }
-            const newBoard = this.state.boardData
-
-            this.setState({ boardData: newBoard, activePlayer: this.state.activePlayer })
-          } }>
-            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}>
-            </View>
+            <TouchableHighlight onPress={ () => this.touchBoard(0,0) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
+                { this.state.boardData[0][0] == 1 && <Text> o </Text> }
+                { this.state.boardData[0][0] == 2 && <Text> x </Text> }
+              </View>
             </TouchableHighlight>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-
-            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            <TouchableHighlight onPress={ () => this.touchBoard(0,1) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} >
+                { this.state.boardData[0][1] == 1 && <Text> o </Text> }
+                { this.state.boardData[0][1] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={ () => this.touchBoard(0,2) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}>
+                { this.state.boardData[0][2] == 1 && <Text> o </Text> }
+                { this.state.boardData[0][2] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
           </View>
           <View style={{
           flex: 1,
@@ -69,9 +89,24 @@ class Board extends Component {
           justifyContent: 'center',
           marginBottom: 40
           }}>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+            <TouchableHighlight onPress={ () => this.touchBoard(1,0) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}>
+                { this.state.boardData[1][0] == 1 && <Text> o </Text> }
+                { this.state.boardData[1][0] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={ () => this.touchBoard(1,1) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} >
+                { this.state.boardData[1][1] == 1 && <Text> o </Text> }
+                { this.state.boardData[1][1] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={ () => this.touchBoard(1,2) }>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} >
+                { this.state.boardData[1][2] == 1 && <Text> o </Text> }
+                { this.state.boardData[1][2] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
           </View>
           <View style={{
           flex: 1,
@@ -79,12 +114,27 @@ class Board extends Component {
           justifyContent: 'center',
           marginBottom: 40
           }}>
-            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+            <TouchableHighlight onPress={ () => this.touchBoard(2,0) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}>
+                { this.state.boardData[2][0] == 1 && <Text> o </Text> }
+                { this.state.boardData[2][0] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={ () => this.touchBoard(2,1) }>
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} >
+                { this.state.boardData[2][1] == 1 && <Text> o </Text> }
+                { this.state.boardData[2][1] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={ () => this.touchBoard(2,2) }>
+            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} >
+                { this.state.boardData[2][2] == 1 && <Text> o </Text> }
+                { this.state.boardData[2][2] == 2 && <Text> x </Text> }
+              </View>
+            </TouchableHighlight>
           </View>
         
-        <Button onPress={ () => this.props.navigation.navigate('GameOver') } title="GameOver" />
+        <Button style={ { marginTop:100  }} onPress={ () => this.props.navigation.navigate('GameOver') } title="GameOver" />
       </View>
     );
   }
